086505a65d0fbd9fcdc5b64d67a976754db7e21c
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-03-18 13:07:14 +0530

    added aws s3 service

--- src/main/java/com/hardik/flenderson/repository/CompanyEventRepository.java
+++ src/main/java/com/hardik/flenderson/repository/CompanyEventRepository.java
@@ -1,12 +1,12 @@
 package com.hardik.flenderson.repository;
 
 
-import com.hardik.flenderson.entity.CompanyEvent;
-
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.hardik.flenderson.entity.CompanyEvent;
 
 @Repository
 public interface CompanyEventRepository extends JpaRepository<CompanyEvent, UUID>{
--- src/main/java/com/hardik/flenderson/repository/EmployeeSocialRepository.java
+++ src/main/java/com/hardik/flenderson/repository/EmployeeSocialRepository.java
@@ -1,12 +1,11 @@
 package com.hardik.flenderson.repository;
 
-import org.springframework.stereotype.Repository;
-
-import com.hardik.flenderson.entity.EmployeeSocial;
-
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hardik.flenderson.entity.EmployeeSocial;
 
 @Repository
 public interface EmployeeSocialRepository extends JpaRepository<EmployeeSocial, UUID>{
