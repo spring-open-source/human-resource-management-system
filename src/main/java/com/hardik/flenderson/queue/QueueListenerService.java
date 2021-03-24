7dca4da1124063419c746da23061af189cce2df9
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-03-24 13:51:44 +0530

    added queue listener logic for attendance

--- src/main/java/com/hardik/flenderson/queue/QueueListenerService.java
+++ src/main/java/com/hardik/flenderson/queue/QueueListenerService.java
@@ -1,21 +1,46 @@
 package com.hardik.flenderson.queue;
 
+import java.io.IOException;
+import java.time.LocalDate;
+
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import com.hardik.flenderson.dto.AttendanceWrapperDto;
 import com.hardik.flenderson.queue.configuration.QueueConfiguration;
+import com.hardik.flenderson.service.AttendanceRecordService;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 @EnableConfigurationProperties(QueueConfiguration.class)
 public class QueueListenerService {
+	
+	private final AttendanceRecordService attendanceRecordService; 
 
 
 	@SqsListener("flenderson-attendance-listener-queue")
 	public void loadMessageFromSQS(String message) {
-		
+		AttendanceWrapperDto attendanceWrapperDto = null;
+		try {
+			attendanceWrapperDto = new ObjectMapper()
+					.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+					.registerModule(new ParameterNamesModule()).registerModule(new Jdk8Module())
+					.registerModule(new JavaTimeModule()).readValue(message, AttendanceWrapperDto.class);
+		} catch (IOException e) {
+			log.error("Failed To Convert JSON to Object");
+			log.error(e.toString());
+		}
+		attendanceRecordService.saveAll(attendanceWrapperDto);
+		log.info("Successfully Pushed Data In Attendance Record Table For " + LocalDate.now().minusDays(1));
 	}
 }
--- src/main/java/com/hardik/flenderson/service/AttendanceRecordService.java
+++ src/main/java/com/hardik/flenderson/service/AttendanceRecordService.java
@@ -1,7 +1,11 @@
 package com.hardik.flenderson.service;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
+import com.hardik.flenderson.dto.AttendanceWrapperDto;
+import com.hardik.flenderson.entity.AttendanceRecord;
 import com.hardik.flenderson.repository.AttendanceRecordRepository;
 
 import lombok.AllArgsConstructor;
@@ -11,5 +15,19 @@ import lombok.AllArgsConstructor;
 public class AttendanceRecordService {
 	
 	private final AttendanceRecordRepository attendanceRecordRepository;
+
+	public void saveAll(AttendanceWrapperDto attendanceWrapperDto) {
+		final var attendanceRecords = new ArrayList<AttendanceRecord>();
+		attendanceWrapperDto.getData().forEach(dailyAttendanceDto -> {
+			final var attendanceRecord = new AttendanceRecord();
+			attendanceRecord.setCompanyId(dailyAttendanceDto.getCompanyId());
+			attendanceRecord.setEmployeeId(dailyAttendanceDto.getEmployeeId());
+			attendanceRecord.setDate(dailyAttendanceDto.getDate());
+			attendanceRecord.setMarked(dailyAttendanceDto.isMarked());
+			attendanceRecord.setPresent(dailyAttendanceDto.isPresent());
+			attendanceRecords.add(attendanceRecord);
+		});
+		attendanceRecordRepository.saveAll(attendanceRecords);
+	}
 
 }
