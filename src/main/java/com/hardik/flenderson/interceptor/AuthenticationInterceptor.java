304bc7b844b9adb8ba23a0f32c17ae93afff4bf0
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-04-14 12:55:58 +0530

    fixed cors issue by not throwing exception in interceptor

--- src/main/java/com/hardik/flenderson/interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/hardik/flenderson/interceptor/AuthenticationInterceptor.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import com.hardik.flenderson.enums.ExceptionMessage;
 import com.hardik.flenderson.exception.AccessTokenExpiredException;
 import com.hardik.flenderson.exception.AuthenticationBearerStrategyNotUsedException;
-import com.hardik.flenderson.exception.MissingAccessTokenAndUserIdException;
 import com.hardik.flenderson.interceptor.dto.UserDetailDto;
 import com.hardik.flenderson.utility.JwtUtility;
 
@@ -33,18 +32,18 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 			throws Exception {
 		final var authentication = (String) request.getHeader("Authentication");
 		final var userId = (String) request.getHeader("User-Id");
-		if (authentication == null || userId == null)
-			throw new MissingAccessTokenAndUserIdException(
-					ExceptionMessage.MISSING_ACCESS_TOKEN_AND_USER_ID.getMessage());
-		final var idTokenArray = authentication.split(" ");
-		if (!idTokenArray[0].toUpperCase().equals("BEARER") || List.of(idTokenArray).size() != 2)
-			throw new AuthenticationBearerStrategyNotUsedException(
-					ExceptionMessage.AUTHENTICATION_BEARER_STRATEGY_NOT_USED.getMessage());
-		final var idToken = idTokenArray[1];
-		if (JwtUtility.isExpired(idToken))
-			throw new AccessTokenExpiredException(ExceptionMessage.ACCESS_TOKEN_EXPIRED.getMessage());
-		threadLocal.set(UserDetailDto.builder().userId(UUID.fromString(userId))
-				.accountType(JwtUtility.getAccountType(idToken)).build());
+		
+		if (authentication != null) {
+			final var idTokenArray = authentication.split(" ");
+			if (!idTokenArray[0].toUpperCase().equals("BEARER") || List.of(idTokenArray).size() != 2)
+				throw new AuthenticationBearerStrategyNotUsedException(
+						ExceptionMessage.AUTHENTICATION_BEARER_STRATEGY_NOT_USED.getMessage());
+			final var idToken = idTokenArray[1];
+			if (JwtUtility.isExpired(idToken))
+				throw new AccessTokenExpiredException(ExceptionMessage.ACCESS_TOKEN_EXPIRED.getMessage());
+			threadLocal.set(UserDetailDto.builder().userId(UUID.fromString(userId))
+					.accountType(JwtUtility.getAccountType(idToken)).build());
+		}
 		return true;
 	}
 }
